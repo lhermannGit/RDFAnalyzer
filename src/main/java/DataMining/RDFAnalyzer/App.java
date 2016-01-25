1c5cce55879c82965b2a46fe4844a09eb98a9246
Celania <garet@gmx.net>
2016-01-25 13:44:03 +0100

    updated file name

--- src/main/java/DataMining/RDFAnalyzer/App.java
+++ src/main/java/DataMining/RDFAnalyzer/App.java
@@ -8,35 +8,31 @@ import input.DisjointSet;
 import input.Reader;
 import threads.BagWorker;
 import threads.Threadpool;
-import util.FilePathFormatter;
 
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-    	Threadpool threadpool = new Threadpool();
-    	String crawl = "\\workspace\\java\\Uni\\data0";
-    	String homeDir = System.getProperty("user.home");
-		Reader reader=new Reader(new File(homeDir+FilePathFormatter.setSeparators(crawl)));
-		
+public class App {
+	public static void main(String[] args) {
+		Threadpool threadpool = new Threadpool();
+		String crawl = "data0";
+		String homeDir = System.getProperty("user.home");
+		Reader reader = new Reader(
+				new File(/* homeDir+ FilePathFormatter.setSeparators( */crawl/* ) */));
+
 		try {
-			List<input.DisjointSet>sets=reader.read();
+			List<input.DisjointSet> sets = reader.read();
 			int bagID = 0;
-			for (DisjointSet set :sets){
+			for (DisjointSet set : sets) {
 				System.out.println(set.getSize());
 				threadpool.execute(new BagWorker(set, bagID, 0));
 				bagID++;
 			}
-			
-			
 
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-    }
+	}
 }
