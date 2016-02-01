09e73196fe21cca8b4a7fe9bcec32a9222b71c45
Jonas <Jonas@Jonas-PC>
2016-02-01 17:21:48 +0100

    adding classpath debug info

--- src/main/java/DataMining/RDFAnalyzer/App.java
+++ src/main/java/DataMining/RDFAnalyzer/App.java
@@ -14,12 +14,23 @@ import input.Reader;
 import threads.BagWorker;
 import threads.Threadpool;
 
+import java.net.URL;
+import java.net.URLClassLoader;
 
 public class App {
 	
 	
 	
 	public static void main(String[] args) {
+		
+		ClassLoader cl = ClassLoader.getSystemClassLoader();
+
+        URL[] urls = ((URLClassLoader)cl).getURLs();
+
+        for(URL url: urls){
+        	System.out.println(url.getFile());
+        }
+		
 		Threadpool threadpool = new Threadpool();
 		Logger LOG = LogManager.getLogger(App.class.getName());
 		
